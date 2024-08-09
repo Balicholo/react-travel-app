f98e2433f6744b7bc8a7bea5c83e778253046fea
Balicholo <wisdombalicholo@gmail.com>
2024-08-09 11:52:43 +0200

    user-side

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header';
+import AuthHeader from './components/AuthHeader';
+import Footer from './components/Footer';
+import HomePage from './components/HomePage';
+import FindVanPage from './components/FindVanPage';
+import AboutPage from './components/AboutPage';
+import SignInPage from './components/SignInPage';
+import ContactPage from './components/ContactPage';
 import './App.css';
+import VanDetailsPage from './components/VanDetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <AppRoutes />
+      </div>
+    </Router>
+  );
+}
+
+function AppRoutes() {
+  const location = useLocation();
+
+  const isAuthPage = location.pathname === '/sign-in' || location.pathname === '/contact';
+
+  return (
+    <>
+      {isAuthPage ? <AuthHeader /> : <Header />}
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/find-van' element={<FindVanPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/sign-in' element={<SignInPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/van-details/:id' element={<VanDetailsPage />} /> {/* Add this route */}
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/AuthHeader.js
@@ -0,0 +1,22 @@
+// src/components/AuthHeader.js
+import React from 'react';
+import { Link,useLocation } from 'react-router-dom';
+
+function AuthHeader() {
+  const location = useLocation();
+  return (
+    <nav className='navbar'>
+      <div className='logo'>
+        <Link to='/' className='logo-link'>#CarRental</Link>
+      </div>
+      <div className='nav-links'>
+        <Link to='/sign-in' className={location.pathname === "/sign-in" ? "active":""}style={{textDecoration: location.pathname === "/sign-in" ? "underline":"none"}}>Host</Link>
+        <Link to='/about' className={location.pathname === "/about" ? "active":""}style={{textDecoration: location.pathname === "/about" ? "underline":"none"}}>About</Link>
+        <Link to='/find-van' className={location.pathname === "/find-van" ? "active":""}style={{textDecoration: location.pathname === "/find-van" ? "underline":"none"}}>Vans</Link>
+        <Link to='/contact'><i class="ri-account-circle-line"></i></Link>
+      </div>
+    </nav>
+  );
+}
+
+export default AuthHeader;
