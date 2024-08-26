9c141a33077f5bfc794bd9a3da89785a9f4281c9
Balicholo <wisdombalicholo@gmail.com>
2024-08-26 15:01:00 +0200

    intergration

--- src/components/AuthHeader.js
+++ src/components/AuthHeader.js
@@ -13,7 +13,7 @@ function AuthHeader() {
         <Link to='/sign-in' className={location.pathname === "/sign-in" ? "active":""}style={{textDecoration: location.pathname === "/sign-in" ? "underline":"none"}}>Host</Link>
         <Link to='/about' className={location.pathname === "/about" ? "active":""}style={{textDecoration: location.pathname === "/about" ? "underline":"none"}}>About</Link>
         <Link to='/find-van' className={location.pathname === "/find-van" ? "active":""}style={{textDecoration: location.pathname === "/find-van" ? "underline":"none"}}>Vans</Link>
-        <Link to='/contact'><i class="ri-account-circle-line"></i></Link>
+        <Link to='https://travel-app-host.vercel.app/'><i class="ri-account-circle-line"></i></Link>
       </div>
     </nav>
   );
